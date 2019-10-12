fde0e202d5d4a19235bdfd4df7493a01442fcb62
NGPHAN310707 <npnallstar@gmail.com>
2019-10-12 21:57:50 +0700

    Create turtlehinhvuong.py

---
+++ turtlehinhvuong.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+#shape('turtle')
+ABCrua = 10 #input('how far?')
+
+turtle.penup()
+turtle.forward(20)
+
+turtle.pendown()
+turtle.forward(90)
+turtle.left(120)
+turtle.forward(90)
+turtle.left(120)
+turtle.forward(90)
+
+for i in range (3):
+    turtle.forward(120)
+    turtle.left(120)
+    
