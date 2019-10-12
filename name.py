4b93d3c4ed008e7da4f61fb28de157b8d4aa6fe2
NGPHAN310707 <npnallstar@gmail.com>
2019-10-12 21:23:31 +0700

    turtle test

---
+++ name.py
@@ -0,0 +1,22 @@
+
+nameschool=input("nameschool?")
+age=input("age?")
+address=input("address?")
+address="I'm from Vietnam"
+nameschool="My name is Nguyen and I'm a student at CVA high school"
+age=15
+print(address, nameschool, age)
+print(age+5)
+print(nameschool)
+print('my name is ',Nguyen, 'I come from ',nameschool)
+
+
+
+
+
+
+
+
+
+
+
---
+++ season 2/name.py
@@ -0,0 +1,4 @@
+name="Nguyen"
+address="Vietnam"
+age="15"
+print(name, address, age)
