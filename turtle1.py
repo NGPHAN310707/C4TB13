5c503d4b8e9b816b1f887b70cade67ef85f86553
NGPHAN310707 <npnallstar@gmail.com>
2019-10-12 21:41:47 +0700

    Update turtle1.py

--- turtle1.py
+++ turtle1.py
@@ -1,2 +1,16 @@
 import turtle
 
+turtle.circle(100)
+for i in range (10):
+    turtle.right(36)
+    turtle.forward(100)
+
+for i in range (20):
+    turtle.right(18)
+    turtle.forward(50)
+for i in range (720):
+    turtle.right(0.5)
+    turtle.forward(2)
+
+
+    
