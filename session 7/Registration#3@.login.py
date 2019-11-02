4b2f7c46ebd2cd0827bfb151191ec2c8986efd36
NGPHAN310707 <npnallstar@gmail.com>
2019-11-03 01:25:42 +0700

    Update Registration#3@.login.py

--- session 7/Registration#3@.login.py
+++ session 7/Registration#3@.login.py
@@ -2,7 +2,7 @@ while True:
     Username = input("Your username?")
     codep = input("Your top-secret password?")
     if len(codep) < 8 and codep.isalpha:
-        print("must have more than 8 letters")
+        print("must have more than 8 characters")
         continue
     reenterpassword = input("Enter the same thing here?")
     if codep != reenterpassword:
