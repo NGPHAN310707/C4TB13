a3d7ac5ba259e6f8253af39b7b2135ac36b66bbd
NGPHAN310707 <npnallstar@gmail.com>
2019-11-03 01:05:19 +0700

    session 7

---
+++ session 7/Registration#2 re-enter.py
@@ -0,0 +1,9 @@
+while True:
+    Username = input("Enter context?")
+    password = input("Please, your password?")
+    reenterpassword = input("re-enter your password?")
+    if password != reenterpassword:
+        print("does not match")
+    email = input("EMAIL?")
+    break
+print('You are all set!') 
