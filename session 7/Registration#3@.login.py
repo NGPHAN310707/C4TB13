e937cf37ad416bbefb70ce75a93734c197375d1c
NGPHAN310707 <npnallstar@gmail.com>
2019-11-03 01:20:53 +0700

    Update Registration#3@.login.py

---
+++ session 7/Registration#3@.login.py
@@ -0,0 +1,16 @@
+while True:
+    Username = input("Your username?")
+    codep = input("Your top-secret password?")
+    if len(codep) < 8 and codep.isalpha:
+        print("must have more than 8 letters")
+        continue
+    reenterpassword = input("Enter the same thing here?")
+    if codep != reenterpassword:
+        print("TRY AGAIN")
+        continue
+    email = input("your email?")
+    if '@gmail.com' in email:
+        print('Well-done')
+        continue
+    else:
+        print("You failed in this attempt.")
