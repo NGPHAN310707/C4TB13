826b3650c5f7579a5f2d22ec070db223a8849254
NGPHAN310707 <npnallstar@gmail.com>
2019-11-10 01:01:43 +0700

    Create list extra.py

---
+++ session 8/list extra.py
@@ -0,0 +1,6 @@
+toithich = ['Nike Blazer','Jordan DNA','CHAMPION']
+toithich.append('Netflix')
+toithich.append("Iphone")
+toithich.append("macbook")
+for i, toithich in enumerate(toithich):
+    print(toithich)
