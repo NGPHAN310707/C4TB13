e9dea0cc33bdff208f6d2009d77792218879cf7f
NGPHAN310707 <npnallstar@gmail.com>
2019-11-10 18:01:45 +0700

    Update PART 3 I.py

---
+++ session 9/PART 3 I.py
@@ -0,0 +1,7 @@
+list = ['5', '12', '6', '61', '124']
+num = int(input("Enter a number: "))
+sum = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
