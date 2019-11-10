e215851056a241304f3cd23286ad0d3151941e7a
NGPHAN310707 <npnallstar@gmail.com>
2019-11-10 11:30:25 +0700

    Update PART 2 II.py

---
+++ session 9/PART 2 II.py
@@ -0,0 +1,15 @@
+
+
+list = ['blue','red','black','green']
+x = int(input('Enter your number? '))
+if x == 0:
+    list.pop(1)
+    list.remove('blue')
+    list.remove('green')
+    print(list)
+
+list = ['blue','red','black','green']
+list = [x for x in li if x%2!=0]
+print (list)
+
+len(list)
