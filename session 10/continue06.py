91ecd854f2b647e7444b33f322cf339cc19070cb
NGPHAN310707 <npnallstar@gmail.com>
2019-11-24 00:06:03 +0700

    Create continue06.py

---
+++ session 10/continue06.py
@@ -0,0 +1,15 @@
+a = {'name' : 'Nguyen',
+    'role' : 'waiter',
+    'hour' : 12,
+    'salary per hours($)' : 0.8}                        
+b = {'name' : 'Phan',
+    'role' : 'cook',
+    'hour' : 24,
+    'salary per hours($)' : 1.5}                                        
+c = {'name' : 'Thi',
+    'role' : 'manager',
+    'hour' : 20,
+    'salary per hours($)' : 2}
+nhanvien = [a,b,c]
+nhanvien.pop(2)
+print(nhanvien)
