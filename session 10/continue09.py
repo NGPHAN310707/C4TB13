7d5e754d95c9f52bdf4414d79f2dcc581ae8b407
NGPHAN310707 <npnallstar@gmail.com>
2019-11-24 00:19:59 +0700

    Create continue09.py

---
+++ session 10/continue09.py
@@ -0,0 +1,10 @@
+a = {
+    'Frozen 2' : '2019 release, actors : Elsa ,Anna ,Olaf',
+}
+b = { 
+    'END GAME' : 'April 22 2019 (release date), actors : blackwindow, Ironman, Thor'
+}    
+a['made in'] = 'USA'
+b['made in'] = 'USA'
+S = [a,b]
+print(S)
