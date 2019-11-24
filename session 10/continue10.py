3bb12c27682a3d7f2d5d92cbb98ec3f7a4b1f1dc
NGPHAN310707 <npnallstar@gmail.com>
2019-11-24 09:33:46 +0700

    Create continue10.py

---
+++ session 10/continue10.py
@@ -0,0 +1,9 @@
+movie = {
+    'name' : 'END GAME',
+    'address' : 'April 22 2019 in Scotland Norway',
+    'actors' : ['Captain America','Hulk']
+}
+movie['cinemas'] = 'cannot be defined'
+for i,a in enumerate(movie):
+    print(i+1,a,movie[a],sep = '-')
+
